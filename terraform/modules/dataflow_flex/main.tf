10ffbfb5725f8f04e5196dc705229f0771cbea0f
Parag Ghosh <paragpratim@gmail.com>
2025-12-16 16:32:02 +0100

    fix: Refactor Dataflow Flex Template job configuration for clarity and consistency

--- terraform/modules/dataflow_flex/main.tf
+++ terraform/modules/dataflow_flex/main.tf
@@ -16,34 +16,31 @@ resource "google_storage_bucket_object" "flex_template_spec" {
 }
 
 # Dataflow Flex Template Job Launch
-resource "google_dataflow_flex_template_job" "kafka_to_bq" {
-  project = var.project_id
-  region  = var.region
+resource "google_dataflow_flex_template_job" "dataflow_flex_job" {
+  provider = google-beta
+  project  = var.project_id
+  region   = var.region
 
   name                    = "${var.pipeline_name}-flex-job"
   container_spec_gcs_path = google_storage_bucket_object.flex_template_spec.self_link
 
+  subnetwork              = var.dataflow_subnetwork
+  service_account_email   = var.dataflow_service_account
+  staging_location        = var.dataflow_staging_location
+  temp_location           = var.dataflow_temp_location
+  num_workers             = 2
+  max_workers             = 2
+  ip_configuration        = "WORKER_IP_PRIVATE"
+  enable_streaming_engine = true
+
   parameters = {
-    pipelineName   = var.pipeline_name
-    subnetwork     = var.dataflow_subnetwork
-    serviceAccount = var.dataflow_service_account
-    jobName        = "${var.pipeline_name}-${formatdate("YYYYMMDD-hhmmss", timestamp())}"
-    runner         = "DataflowRunner"
-    streaming      = "true"
+    pipelineName = var.pipeline_name
+    jobName      = "${var.pipeline_name}-${formatdate("YYYYMMDD-hhmmss", timestamp())}"
+    runner       = "DataflowRunner"
+    streaming    = "true"
     # Add more parameters as needed
   }
 
-  environment = {
-    tempLocation            = var.dataflow_temp_location
-    stagingLocation         = var.dataflow_staging_location
-    maxWorkers              = 2
-    numWorkers              = 2
-    noPublicIps             = true
-    enable-streaming-engine = true
-    use-streaming-engine    = true
-  }
-
-  on_delete             = "cancel"
-  replace_job_on_update = "true"
-  update_mode           = var.update_mode
+  on_delete                    = var.update_mode
+  skip_wait_on_job_termination = false
 }
