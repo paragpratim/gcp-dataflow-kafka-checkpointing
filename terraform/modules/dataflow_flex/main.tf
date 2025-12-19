5fc47dab804c342e37a224ed82931121d58ed629
Parag Ghosh <paragpratim@gmail.com>
2025-12-19 11:44:56 +0100

    fix: Correct jobName formatting in Dataflow Flex template parameters

--- terraform/modules/dataflow_flex/main.tf
+++ terraform/modules/dataflow_flex/main.tf
@@ -37,7 +37,7 @@ resource "google_dataflow_flex_template_job" "dataflow_flex_job" {
 
   parameters = {
     pipelineName = var.pipeline_name
-    jobName      = "${var.pipeline_name}-${formatdate("YYYYMMDD-hhmmss", timestamp())}"
+    jobName      = "${var.pipeline_name}-${replace(replace(formatdate("yyyyMMdd-HHmmss", timestamp()), ":", ""), "_", "")}"
     runner       = "DataflowRunner"
     streaming    = "true"
     # Add more parameters as needed
