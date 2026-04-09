3be7156f97f83a59973fe5ec66380f40fac1f486
Parag Ghosh <paragpratim@gmail.com>
2026-04-09 23:03:25 +0200

    feat: Add database edition to Firestore database resource for Kafka checkpointing

--- terraform/modules/dataflow/main.tf
+++ terraform/modules/dataflow/main.tf
@@ -174,10 +174,11 @@ resource "google_compute_router_nat" "dataflow_nat" {
 
 # Create Firestore database for Kafka checkpointing
 resource "google_firestore_database" "dataflow_kafka_checkpointing_firestore" {
-  name        = "dataflow-kafka-checkpointing"
-  project     = var.project_id
-  location_id = var.region
-  type        = "FIRESTORE_NATIVE"
+  name             = "dataflow-kafka-checkpointing"
+  project          = var.project_id
+  location_id      = var.region
+  type             = "FIRESTORE_NATIVE"
+  database_edition = "STANDARD"
 
   depends_on = [google_project_service.required_apis]
 }
