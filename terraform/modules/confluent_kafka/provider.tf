9a76850c4fdf6ae08c7444eab9f140c40327d958
Parag Ghosh <paragpratim@gmail.com>
2026-04-17 23:36:09 +0200

    fix: Comment out GITHUB_TOKEN in Terraform workflow and update provider configuration

--- terraform/modules/confluent_kafka/provider.tf
+++ terraform/modules/confluent_kafka/provider.tf
@@ -1,12 +1,12 @@
-terraform {
-  required_providers {
-    confluent = {
-      source  = "confluentinc/confluent"
-      version = "~> 2.0"
-    }
-    github = {
-      source  = "integrations/github"
-      version = "~> 6.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     confluent = {
+#       source  = "confluentinc/confluent"
+#       version = "~> 2.0"
+#     }
+#     github = {
+#       source  = "integrations/github"
+#       version = "~> 6.0"
+#     }
+#   }
+# }
